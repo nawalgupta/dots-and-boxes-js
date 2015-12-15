c1bfd1cfdbf4eaaa078508fb4a22e29877787f37
Alan Cohengit <alan.cohen@nyu.edu>
2015-12-15 15:40:27 -0600

    cleaned up code and added comment

--- js/app.js
+++ js/app.js
@@ -34,21 +34,17 @@ function drawGrid(distanceBtwPts) {
 	var width = CONTEXT.canvas.width;
 	var height = CONTEXT.canvas.height;
 
-	var x = 0;
-	var y = 0;
-
-	for (var x = 1; x < width - distanceBtwPts; x++) {
-		
-		for (var y = 1; y < height - distanceBtwPts; y++) {
-			
+	// We want a 1 pixel offset at the start to avoid half-drawn circles
+	// at the edge of the canvas.  The 1 pixel offset at the end is
+	// to keep the board symetric in the canvas.
+	for (var x = 1; x < width - 1; x++) {
+		for (var y = 1; y < height - 1; y++) {
 			if (x % distanceBtwPts == 0 && y % distanceBtwPts == 0) {
 				CONTEXT.beginPath();
 				CONTEXT.arc(x, y, 1, 0, 2 * Math.PI, true);
 				CONTEXT.fill();
 			};
-
 		};
-
 	};
 };
 
